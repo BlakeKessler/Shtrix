496c996a2aeba42739a4d63d9b0448b1cbea6535
BlakeKessler <blakekessler123@gmail.com>
2025-07-07 15:07:07 -0400

    fix piece rotation

--- app/header/Piece.hpp
+++ app/header/Piece.hpp
@@ -4,7 +4,7 @@
 #include "Shtrix.hpp"
 
 struct shtrix::Piece {
-   uint16 shape;
+   uint64 shape;
    Color c;
 
    void rotR();
@@ -16,13 +16,13 @@ struct shtrix::Piece {
 
 namespace shtrix {
    constexpr Piece PIECES[] {
-      {0x4444,    CYAN}, //LONGBAR
-      {0x0226,    BLUE}, //J
-      {0x0446,   WHITE}, //L
-      {0x0660,  YELLOW}, //SQUARE
-      {0x0360,   GREEN}, //S
-      {0x0270, MAGENTA}, //T
-      {0x0630,     RED}  //Z
+      {0x44440F00222200F0,    CYAN}, //LONGBAR
+      {0x8E0064400E2044C0,    BLUE}, //J
+      {0x44600E80C4402E00,   WHITE}, //L
+      {0x0660066006600660,  YELLOW}, //SQUARE
+      {0xC60026400C604C80,   GREEN}, //S
+      {0x4E0046400E404C40, MAGENTA}, //T
+      {0x6C00462006C08C40,     RED}  //Z
    };
 }
 
